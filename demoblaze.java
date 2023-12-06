0741e12aba4791f39b239de252f7bb3391846fb8
Shivanikshivi <97271307+Shivanikshivi@users.noreply.github.com>
2023-12-06 17:54:19 +0530

    first commit

---
+++ demoblaze.java
@@ -0,0 +1,32 @@
+package selenium;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class demoblaze {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver",
+	            "C:\\Users\\user\\eclipse-workspace\\Java_project1\\src\\selenium\\demoblaze.java");
+	        
+	        WebDriver driver = new ChromeDriver();
+	 
+	        
+	        driver.manage().window().maximize();
+	        driver.navigate().to("https://www.demoblaze.com/");
+	        
+	        if (driver.title == 'Store')
+	        {
+	          System.out.println("Page landed on correct website")
+	        }
+	        else
+	        {
+	            System.out.println("Page not landed on correct website")
+	        }
+
+	        driver.quit();
+	}
+}
+	
+	
+
+
